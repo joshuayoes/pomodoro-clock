1dd07a52bc83d2e842770a39e0ca285b0f1b1410
Joshua Yoes <>
2020-01-25 16:36:01 -0800

    Dependencies and ESLint/Prettier setup

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['airbnb', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  rules: {
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'import/no-extraneous-dependencies': [2, { devDependencies: ['**/test.tsx', '**/test.ts'] }],
+    '@typescript-eslint/indent': [2, 2],
+    'import/extensions': ['error', 'never'],
+    'spaced-comment': ['error', 'always', {
+      'markers': ['/'],
+    }],
+    'linebreak-style': 0    
+  },
+};
