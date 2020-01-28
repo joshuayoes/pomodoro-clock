526d0b8d6429850fe6608f19ce7e2dea870a67ed
Joshua Yoes <>
2020-01-27 20:52:34 -0800

    Setup redux store

---
+++ src/redux/reducers/index.ts
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import timer from './timerReducer';
+
+export default combineReducers({
+  timer,
+});
---
+++ src/redux/store.ts
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-underscore-dangle */
+import { createStore } from 'redux';
+import rootReducer from './reducers';
+
+const store = createStore(
+  rootReducer,
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__
+    && (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+export default store;
