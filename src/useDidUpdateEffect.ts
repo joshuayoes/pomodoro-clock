a250dce4fa8067483c1ab281d938d7e26c07a8c4
Joshua Yoes <>
2020-03-23 12:33:29 -0700

    Added comment

--- src/useDidUpdateEffect.ts
+++ src/useDidUpdateEffect.ts
@@ -1,5 +1,7 @@
 import { useEffect, useRef } from 'react';
 
+// Motiviation for hook outlined by following thread
+// https://stackoverflow.com/a/53180013
 export default function useDidUpdateEffect(fn: () => void, inputs: string[]): void {
   const didMountRef = useRef(false);
 
