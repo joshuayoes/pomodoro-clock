1407ed8497cbd0df21b82ab96db296120481254d
Joshua Yoes <>
2020-03-23 11:59:32 -0700

    Fire timer sound on update

--- src/components/TimerDisplay.tsx
+++ src/components/TimerDisplay.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import secondsToClock from '../utilityFunctions';
 import { AppStore } from '../redux/store';
@@ -7,10 +7,32 @@ const TimerDisplay: React.FC = () => {
   const currentTimerType = useSelector((state: AppStore) => state.timer.currentTimerType);
   const clock = useSelector((state: AppStore) => secondsToClock(state.timer.timer));
 
+  // prevents sound from firing on intial mount
+  const [notFirstMount, setNotFirstMount] = useState(false);
+
+  useEffect(() => {
+    if (notFirstMount) {
+      // eslint-disable-next-line no-undef
+      const alarm = document.getElementById('beep') as HTMLAudioElement;
+      alarm.play();
+    }
+
+    // allows sound to play on future state changes
+  }, [currentTimerType]);
+
+  useEffect(() => setNotFirstMount(true), []);
+
+
   return (
     <>
       <section id="timer-label">{currentTimerType}</section>
       <section id="time-left">{clock}</section>
+
+      {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
+      <audio id="beep">
+        <source src="./alarm.mp3" type="audio/mpeg" />
+        Your browser does not support the audio tag.
+      </audio>
     </>
   );
 };
